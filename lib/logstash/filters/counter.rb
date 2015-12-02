86d2ebce9024ec96f4c094e2fb52d533a5495a1a
熊凤龙 <fenglongxiong@anjuke.com>
2015-12-02 19:28:55 +0800

    update

--- lib/logstash/filters/counter.rb
+++ lib/logstash/filters/counter.rb
@@ -6,7 +6,7 @@ require "logstash/namespace"
 # message field with whatever you specify in the configuration.
 #
 # It is only intended to be used as an example.
-class LogStash::Filters::Example < LogStash::Filters::Base
+class LogStash::Filters::Counter < LogStash::Filters::Base
 
   # Setting the config_name here is required. This is how you
   # configure this filter from your Logstash config.
@@ -17,10 +17,12 @@ class LogStash::Filters::Example < LogStash::Filters::Base
   #   }
   # }
   #
-  config_name "example"
+  config_name "counter"
   
   # Replace the message with this value.
   config :message, :validate => :string, :default => "Hello World!"
+  config :meter, :validate => :array,:default => []
+  config :cycle, :validate => :number,:default => 60
   
 
   public
@@ -34,7 +36,8 @@ class LogStash::Filters::Example < LogStash::Filters::Base
     if @message
       # Replace the event message with our message as configured in the
       # config file.
-      event["message"] = @message
+      time = event["time"].strftime("%Y-%m-%d %H:%M:%S")
+      event["time"]= time
     end
 
     # filter_matched should go in the last line of our successful code
